08e45010faf1fa345b0c8d72e91bce984c2590a0
Guy Ernest <guy.ernest@gmail.com>
2024-07-06 15:20:50 -0700

    Chat example.

--- hello_world.py
+++ hello_world.py
@@ -1,18 +1,31 @@
+import random
 import time
 
 import mesop as me
 import mesop.labs as mel
 
 
-@me.page(path="/text_to_text", title="Text I/O Example")
-def app():
-  mel.text_to_text(
-    upper_case_stream,
-    title="Text I/O Example",
-  )
+@me.page(
+  path="/chat",
+  title="Mesop Demo Chat",
+)
+
+def page():
+  mel.chat(transform, title="Mesop Demo Chat", bot_user="Mesop Bot")
 
 
-def upper_case_stream(s: str):
-  yield s.capitalize()
-  time.sleep(0.5)
-  yield "Done"
+def transform(input: str, history: list[mel.ChatMessage]):
+  for line in random.sample(LINES, random.randint(3, len(LINES) - 1)):
+    time.sleep(0.3)
+    yield line + " "
+
+
+LINES = [
+  "Mesop is a Python-based UI framework designed to simplify web UI development for engineers without frontend experience.",
+  "It leverages the power of the Angular web framework and Angular Material components, allowing rapid construction of web demos and internal tools.",
+  "With Mesop, developers can enjoy a fast build-edit-refresh loop thanks to its hot reload feature, making UI tweaks and component integration seamless.",
+  "Deployment is straightforward, utilizing standard HTTP technologies.",
+  "Mesop's component library aims for comprehensive Angular Material component coverage, enhancing UI flexibility and composability.",
+  "It supports custom components for specific use cases, ensuring developers can extend its capabilities to fit their unique requirements.",
+  "Mesop's roadmap includes expanding its component library and simplifying the onboarding processs.",
+]
